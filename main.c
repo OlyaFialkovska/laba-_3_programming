1cf232d9dcb92afba1c9e0b3bfe770e08da51143
Olya <106016782+OlyaFialkovska@users.noreply.github.com>
2024-03-18 22:28:18 +0200

    laba 3 function

--- main.c
+++ main.c
@@ -2,41 +2,64 @@
 #include <stdlib.h>
 #include <math.h>
 
-int main()
+float mxAvc ()
 {
+    const int n = 9;
     const float e = 2.71828182846;
-    const int n=9;
-    float a[n][n];
-    float x[n];
-    float p = 1;
-    int i,j,k=0;
-    for( i = 0; i<n; i++)
+    float a[n][n], x[n];
+    float u = 1;
+    printf("MATRIX a:\n");
+    for(int i=1; i<113; i++)
+        printf("-");
+    printf("\n");
+    for(int i = 0; i<n; i++)
     {
-        for( j = 0; j<n; j++)
+        for(int j = 0; j<n; j++)
         {
-            if(i>0)
+            if( i > 0 )
                 a[i][j] = pow(e,(i-j))+ 2.27 * log(i);
-            printf("%-12f",a[i][j]);
-            x[j] = a[i][j];
-            if(x[j]>0 && x[j]!=1)
-            {
-                p = p * log(fabs(x[j]));
-            }
+            printf("%-13.5f",a[i][j]);
         }
         printf("\n");
     }
-    for(j=0; j<n; j++)
+    for(int i=1; i<113; i++)
+        printf("-");
+    printf("\n");
+    for(int j = 0; j<n; j++)
     {
-        for( i=0; i<n; i++)
+        for(int i = 0; i<n; i++)
         {
-            x[i]=a[j][i];
+            x[i] = a[j][i];
         }
     }
-    for(j=0; j<n; j++)
+    printf("VECTOR x:\n");
+    for(int i=1; i<105; i++)
+        printf("-");
+    printf("\n");
+    for(int i = 0; i<=n-1; i++)
     {
-        printf("|%-12f\n",x[j]);
+        printf("%-12.5f",x[i]);
+        if(x[i]>0 && x[i]!=1)
+        {
+            u = u * log(fabs(x[i]));
+        }
     }
-    printf(" Result = %f\n",p);
+    printf("\n");
+    for(int i=1; i<105; i++)
+        printf("-");
+    printf("\n");
+    printf("FUNCTION U:");
+    printf("\n");
+    for(int i=1; i<15; i++)
+        printf("-");
+    printf("\n");
+    return u;
+}
+int main()
+{
+    printf("%f\n", mxAvc ());
+    for(int i=1; i<15; i++)
+        printf("-");
     return 0;
 }
 
