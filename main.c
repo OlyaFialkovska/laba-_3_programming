b28a617628e2bad3c15cad865bf5ca69dc41ea0e
Olya <106016782+OlyaFialkovska@users.noreply.github.com>
2024-03-18 22:29:14 +0200

    protection for a lesson

--- main.c
+++ main.c
@@ -1,65 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#define n 10
 
-float mxAvc ()
-{
-    const int n = 9;
-    const float e = 2.71828182846;
-    float a[n][n], x[n];
-    float u = 1;
-    printf("MATRIX a:\n");
-    for(int i=1; i<113; i++)
-        printf("-");
-    printf("\n");
-    for(int i = 0; i<n; i++)
-    {
-        for(int j = 0; j<n; j++)
-        {
-            if( i > 0 )
-                a[i][j] = pow(e,(i-j))+ 2.27 * log(i);
-            printf("%-13.5f",a[i][j]);
-        }
-        printf("\n");
-    }
-    for(int i=1; i<113; i++)
-        printf("-");
-    printf("\n");
-    for(int j = 0; j<n; j++)
-    {
-        for(int i = 0; i<n; i++)
-        {
-            x[i] = a[j][i];
-        }
-    }
-    printf("VECTOR x:\n");
-    for(int i=1; i<105; i++)
-        printf("-");
-    printf("\n");
-    for(int i = 0; i<=n-1; i++)
-    {
-        printf("%-12.5f",x[i]);
-        if(x[i]>0 && x[i]!=1)
-        {
-            u = u * log(fabs(x[i]));
-        }
-    }
-    printf("\n");
-    for(int i=1; i<105; i++)
-        printf("-");
-    printf("\n");
-    printf("FUNCTION U:");
-    printf("\n");
-    for(int i=1; i<15; i++)
-        printf("-");
-    printf("\n");
-    return u;
-}
 int main()
 {
-    printf("%f\n", mxAvc ());
-    for(int i=1; i<15; i++)
-        printf("-");
-    return 0;
-}
+    char a[n] = {'a','b','k','f','h','i','e','y','p','v'}, b[4];
+    int f0=0,f=0,f1=0,f2=0;
 
+    for(int i =0; i<n; i++)
+    {
+        if(a[i]=='k')
+            f0=i;
+        if(a[i]=='y')
+            f=i;
+        if(a[i]=='i')
+            f1=i;
+        if(a[i]=='v')
+            f2=i;
+    }
+
+    if(f0!=0 && f!=0 && f1!=0 && f2!=0)
+    {
+        /* b[0]=a[f0];
+         b[1]=a[f];
+         b[2]=a[f1];
+         b[3]=a[f2];
+         printf("%c",b);*/
+        printf("yes");
+    }
+    else
+    {
+        printf("no);}
+               return 0;
+    }
